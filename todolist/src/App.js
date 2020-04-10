6b2843c701436c23769a462a45904758cf393361
ngocsondev <ngocsonytb2016@gmail.com>
2020-04-11 01:35:49 +0700

    Setup Component

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// Component Import 
+
+import TaskForm from './components/TaskFrom'
+import Controls from './components/Controls'
+import TaskLists from './components/TaskLists'
+
+class App extends Component {
+  render(){
+    return(
+      <div className="container">
+      <div className="text-center">
+        <h1>Quản Lý Công Việc</h1>
+        <hr />
+      </div>
+      <div className="row">
+        <TaskForm />
+        <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+          <button type="button" className="btn btn-primary">
+            <span className="fa fa-plus mr-5" />
+            Thêm Công Việc
+          </button>
+          <Controls />
+          <TaskLists/>
+        </div>
+      </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
